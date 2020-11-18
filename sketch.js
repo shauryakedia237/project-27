119fc1deeb37176837e60da709d07ab80b13f063
shauryakedia237 <69040024+shauryakedia237@users.noreply.github.com>
2020-11-18 16:44:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint= Matter.Constraint;
 
 function preload()
 {
@@ -17,16 +18,16 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
-	bob1=new bob(800-80,675,40);
-	bob2=new bob(800-40,675,40);
-	bob3=new bob(800,675,40);
-	bob4=new bob(800+40,675,40);
-	bob5=new bob(800+80,675,40);
+	bob1=new Bob(800-80,675,40);
+	bob2=new Bob(800-40,675,40);
+	bob3=new Bob(800,675,40);
+	bob4=new Bob(800+40,675,40);
+	bob5=new Bob(800+80,675,40);
 
 	roof = new Roof(width/2,height/4,width/7,20);
 	
 
-	rope1 = new rope(bob1.body,roof.body,-40*2,0)
+	rope1 = new Rope(bob1.body,roof.body,-40*2,0)
 	
 	Engine.run(engine);
   
